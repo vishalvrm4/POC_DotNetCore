22b4e502fd955a77f7da8a381367eb9868e80c70
vishalvrm4 <vishalvrm4@users.noreply.github.com>
2024-05-28 17:19:54 +0530

    Global Exception Handling-Middleware

---
+++ POC_DotNetCore/GlobalExceptionHandling/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace GlobalExceptionHandling
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An unhandeled exception occurred.");
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        public static Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var response = new
+            {
+                StatusCode=context.Response.StatusCode,
+                Message="An error occured while processing your request",
+                Detailed= ex.Message
+            };
+
+            return context.Response.WriteAsync(JsonSerializer.Serialize(response));
+        }
+    }
+}
--- POC_DotNetCore/POC_JWT_Token/LoginModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace POC_JWT_Token
-{
-    public class LoginModel
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
